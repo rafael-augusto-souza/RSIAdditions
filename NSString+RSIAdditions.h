5b4bc9b6d9e3e749bdb4b455b9140fe42d27bf4d
Rafael Souza <radiaz@gmail.com>
2013-10-02 14:44:43 -0300

    Initial commit with NSString

---
+++ NSString+RSIAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSString+RSIAdditions.h
+//  RSIAdditions_Tests
+//
+//  Created by Rafael Souza on 02/10/13.
+//  Copyright (c) 2013 Rafael Souza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (RSIAdditions)
+
+@end
---
+++ NSString+RSIAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSString+RSIAdditions.m
+//  RSIAdditions_Tests
+//
+//  Created by Rafael Souza on 02/10/13.
+//  Copyright (c) 2013 Rafael Souza. All rights reserved.
+//
+
+#import "NSString+RSIAdditions.h"
+
+@implementation NSString (RSIAdditions)
+
+@end
